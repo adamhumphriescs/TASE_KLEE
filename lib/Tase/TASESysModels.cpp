72a818e5a16c5b7f6377903e92a3912a71d5df4c
Jeremy Gresham <jsg57@duke.edu>
2022-04-20 16:23:10 -0400

    updated

--- lib/Tase/TASESysModels.cpp
+++ lib/Tase/TASESysModels.cpp
@@ -133,6 +133,7 @@ template<> int16_t as(tase_greg_t t){return t.i16;}
 template<> uint16_t as(tase_greg_t t){return t.u16;}
 template<> double as(tase_greg_t t){return t.dbl;}
 template<> char as(tase_greg_t t){return (char) t.u8;}
+/*
 template<> char * as(tase_greg_t t){return (char*) t.u64;}
 template<> wchar_t* as(tase_greg_t t){return (wchar_t*) t.u64;}
 template<> char ** as(tase_greg_t t){return (char**) t.u64;}
@@ -144,7 +145,7 @@ template<> sigset_t* as(tase_greg_t t){return (sigset_t*) t.u64;}
 template<> struct sigaction* as(tase_greg_t t){return (struct sigaction*) t.u64;}
 template<> mbstate_t* as(tase_greg_t t){return (mbstate_t*) t.u64;}
 template<> void* as(tase_greg_t t){return (void*) t.u64;}
-
+*/
 //template<> time_t* as(tase_greg_t t){return (time_t*) t.u64;}
 
 #define _LOG std::cout << "Entering " << __func__ << " at interpCtr " << interpCtr << std::endl;
