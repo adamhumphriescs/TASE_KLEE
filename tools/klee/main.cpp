bf62bc9713a703df0ae827471fd1e9c1d1a39147
Jeremy Gresham <jsg57@duke.edu>
2022-07-27 17:57:19 -0400

    updated

--- tools/klee/main.cpp
+++ tools/klee/main.cpp
@@ -1768,12 +1768,10 @@ static llvm::Module *linkWithUclibc(llvm::Module *mainModule, StringRef libDir) 
 
      auto exe = static_cast<klee::Executor*>(interpreter);
      exe->tase_map(saved_rax, "saved_rax");
-     //exe->tase_map_buf((uint64_t) &pArgc, sizeof(pArgc));
-     //exe->tase_map_buf((uint64_t) pArgv, sizeof(pArgv)*pArgc);
      for(int i = 0; i < pArgc; ++i){
-       //exe->tase_map_buf((uint64_t) pArgv[i], argsizes[i]);
        printf("mapping arg: %s, size: %d\n", pArgv[i], strlen(pArgv[i]+1));
-       exe->tase_map(pArgv[i], "pArgv[" + std::to_string(i) + "]");
+       //exe->tase_map(pArgv[i], "pArgv[" + std::to_string(i) + "]");
+       
      }
 
      transferToTarget(pArgc, pArgv);
